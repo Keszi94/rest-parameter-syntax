6b4bdd36afc615777ce8d5b088917b54194ec482
Eszter Kiskarpati <keszi94@gmail.com>
2024-11-08 21:47:46 +0000

    rest parameter syntax examples

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -3,8 +3,25 @@
  * command node rest-parameter-syntax.js in the terminal
  */
 
-// Regular function call 
 
-// Extra arguments are ignored
+// __~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~  Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("Sum: ", sum);
 
-// Function using ...rest
+// __~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~  Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("sum2: ", sum2);
+
+
+// __~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~__~~  Function using ...rest
+const sumRest = ( a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("sum3: ", sum3);
